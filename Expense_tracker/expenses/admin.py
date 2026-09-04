888f9287aa715300c4b499c645f851af0cc0ab81
AjayGupta-8828 <ajaysonugupta5698@gmail.com>
2026-09-04 20:08:49 +0530

    Update admin panel configuration for expenses app

--- Expense_tracker/expenses/admin.py
+++ Expense_tracker/expenses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Transactions, Budget
 
 # Register your models here.
+# admin.site.register(Transactions)
+# admin.site.register(Budget)
+@admin.register(Transactions)
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'amount', 'types', 'category', 'date', 'created_at')
+    list_filter = ('types', 'category', 'date')
+    search_fields = ('title', 'amount','user__username')
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'category', 'limit')
+    list_filter = ('category',)
+    search_fields = ('limit',)
